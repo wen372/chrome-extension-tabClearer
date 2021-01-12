d2477357870e70da4a344892cff35a6351945f52
Julio <julioballista@gmail.com>
2021-01-12 14:40:50 -0500

    Decomposed functions to background.js

--- tabClearer/popup.js
+++ tabClearer/popup.js
@@ -1,36 +1,23 @@
+/*
+  This is including the helper functions from bgPage. 
+  This includes lock, unlock, and clearTabs. 
+*/
+var bgPage = chrome.extension.getBackgroundPage();
+
+
 //eventlistener
 document.addEventListener('DOMContentLoaded', function () {
 
   //eventlistener for clearTabs button
   var clearTabsButton = document.getElementById('clearTabs');
   clearTabsButton.addEventListener('click', function () {
-    //alert("Hello from your Chrome extension!");
-    chrome.tabs.query({}, function (tabs) {
-      var tabIds = [];
-      tabs.forEach(element => tabIds.push(element.id.toString()));
-      chrome.storage.local.get(tabIds, function (result) {
-        var removeArr = []
-        tabIds.forEach(element => {
-          if (result[element] == undefined) {
-            removeArr.push(parseInt(element));
-          }
-        });
-        chrome.tabs.remove(removeArr, function () { });
-      });
-
-    });
+    bgPage.clearUnlockedTabs();
   }, false);
 
   //eventlistener for lockTab button
   var lockTabButton = document.getElementById('lockTab');
   lockTabButton.addEventListener('click', function () {
-    chrome.tabs.getSelected(null, function (tab) {
-      var currentTabId = tab.id.toString();
-      chrome.storage.local.get(currentTabId, function (result) {
-        result[currentTabId] == undefined ? lock() : unlock();
-      });
-
-    });
+    bgPage.toggleSelectedTabLock();
   }, false);
 
   //eventlistener for expandButton
@@ -95,28 +82,3 @@ document.addEventListener('DOMContentLoaded', function () {
     chrome.tabs.update(tabId, {selected: true}, function(){});
   })
 }, false);
-
-
-
-
-
-//"locks" current tab by saving in storage
-function lock() {
-  chrome.tabs.getSelected(null, function (tab) {
-    var currentId = tab.id.toString();
-    var dictionary = {};
-    dictionary[currentId] = 0;
-    chrome.storage.local.set(dictionary, function () { });
-    chrome.browserAction.setIcon({ tabId: tab.id, path: 'images/locked.png' });
-  });
-}
-
-
-//"unlocks" current tab by removing from storage
-function unlock() {
-  chrome.tabs.getSelected(null, function (tab) {
-    var currentId = tab.id.toString();
-    chrome.storage.local.remove(currentId, function () { });
-    chrome.browserAction.setIcon({ tabId: tab.id, path: 'images/unlocked.png' });
-  });
-}
