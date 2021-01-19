485574a2fc814ef7a9ed090bd39bd374b0344e11
wen372 <wen372@gmail.com>
2021-01-19 02:33:42 -0500

    changed how icon updates on tab update

--- tabClearer/background.js
+++ tabClearer/background.js
@@ -1,17 +1,13 @@
-//listens for "iconCheck" message from visualLockUnlock to set icon for each tab
-chrome.runtime.onMessage.addListener(function(msg) 
-{
-    if(msg.iconCheck){
-    chrome.tabs.getSelected(null, function(tab) {
-        var currentTabId = tab.id.toString();
+//listens for update of tab and updates the icon if locked
+chrome.tabs.onUpdated.addListener(function(tab){
+    var currentTabId = tab.id.toString();
         chrome.storage.local.get(currentTabId,function(result){
             if (result[currentTabId] != undefined){
                 chrome.browserAction.setIcon({tabId: tab.id, path: 'images/locked.png'});
             } 
         });
-      });
-    }
 });
+
 
 
 //listens for tab closing and checks if tab is in locked storage and deletes it from storage if it is
